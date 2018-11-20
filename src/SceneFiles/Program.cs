e4d58fb23be220e525447ff320994075538d9dd2
Chad Cooper <spazmochad@gmail.com>
2018-11-20 17:34:32 +0000

    Argument check now includes directory exists statement

--- src/SceneFiles/Program.cs
+++ src/SceneFiles/Program.cs
@@ -12,7 +12,7 @@ namespace SceneFiles
             var fileAndDirectoryRenamer = new FileAndDirectoryRenamer(fileSystem); 
             var sceneFileRenamer = new GenericSceneRenamer(fileAndDirectoryRenamer);
 
-            if (!fileSystem.File.Exists(args[0]))
+            if (!fileSystem.File.Exists(args[0]) && !fileSystem.Directory.Exists(args[0]))
                 throw new Exception(string.Format("File or directory does not exist: {0}", args[0]));
 
             sceneFileRenamer.RenameFileOrDirectory(args[0]);
