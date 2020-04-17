c9e12b531c972e982f52e0249a446c737c927671
Chad Cooper <spazmochad@gmail.com>
2020-04-17 20:27:42 +0100

    Switch to DI

--- src/SceneFiles/Program.cs
+++ src/SceneFiles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.IO;
 using System.IO.Abstractions;
@@ -9,22 +10,30 @@ namespace SceneFiles
     {
         public static void Main(string[] args)
         {
-            var fileSystem = new FileSystem();
+            var serviceProvider = ServiceCollection().BuildServiceProvider();
 
-            var fileAndDirectoryRenamer = new FileAndDirectoryRenamer(fileSystem); 
-
-            var sceneFileRenamer = new GenericSceneRenamer(fileAndDirectoryRenamer, LoadConfiguration());
+            var sceneRenamer = serviceProvider.GetRequiredService<ISceneRenamer>();
+            var fileSystem = serviceProvider.GetRequiredService<IFileSystem>();
 
             if (!fileSystem.File.Exists(args[0]) && !fileSystem.Directory.Exists(args[0]))
                 throw new Exception(string.Format("File or directory does not exist: {0}", args[0]));
 
-            sceneFileRenamer.RenameFileOrDirectory(args[0]);
+            sceneRenamer.RenameFileOrDirectory(args[0]);
+        }
+
+        private static IServiceCollection ServiceCollection()
+        {
+            return new ServiceCollection()
+                .AddLogging()
+                .AddSingleton(LoadConfiguration())
+                .AddSingleton<IFileSystem, FileSystem>()
+                .AddTransient<IFileAndDirectoryRenamer, FileAndDirectoryRenamer>()
+                .AddTransient<ISceneRenamer, GenericSceneRenamer>();
         }
 
 
         private static AppSettings LoadConfiguration()
         {
-            Console.WriteLine(Directory.GetCurrentDirectory());
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: true,
